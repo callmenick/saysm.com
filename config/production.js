2b561459f7ab5dd3efa2e946ab93602e724dc96a
Nick Salloum <nick.salloum@axiomzen.co>
2016-10-11 14:45:01 -0400

    2016-10-11 14:45:01

--- config/production.js
+++ config/production.js
@@ -1,5 +1,5 @@
 const base = require('./index.js');
 
 module.exports = Object.assign({}, base, {
-  SITE_URL: 'https://callmenick.github.io/static-site'
+  SITE_URL: 'https://saysm.github.io/saysm.com'
 });
